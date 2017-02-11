52a321b2df2e231e995507445c08b915207ae95b
jakemercurio <jake@videoblocks.com>
2017-02-11 10:24:27 -0500

    Verify multilpe streams work for phpTokenizer

--- test/phpTokenizerTest.js
+++ test/phpTokenizerTest.js
@@ -29,7 +29,7 @@ describe('phpTokenizer', () => {
         assert.equal(21, tokens.length);
     });
 
-    it ('should return a stream', () =>{
+    it ('should return a stream', (done) =>{
         const tokenizer = new PhpTokenizer(testCode);
         let data = [];
         let stream = tokenizer.tokenizeStream();
@@ -41,8 +41,47 @@ describe('phpTokenizer', () => {
         });
 
         stream.on('end', () => {
+
+            const expectedArray = tokenizer.tokenizeCode();
+
             assert.equal(21, data.length);
-            assert.equal(tokenizer.tokenizeCode(), data);
+            assert.equal(expectedArray.length, data.length);
+            assert.deepEqual(expectedArray, data);
+            done();
+        });
+
+    });
+
+    it ('should be able to handle multiple streams', (done) =>{
+
+        const tokenizer = new PhpTokenizer(testCode);
+        let data = [];
+        let data2 = [];
+        let stream = tokenizer.tokenizeStream();
+
+        assert.equal('object', typeof stream);
+
+        stream.on('data', (chunk) => {
+            data.push(chunk.toString());
+        });
+
+        stream.on('data', (chunk) => {
+            data2.push(chunk.toString());
+        });
+
+        stream.on('end', () => {
+
+            const expectedArray = tokenizer.tokenizeCode();
+
+            assert.equal(21, data.length);
+            assert.equal(21, data2.length);
+
+            assert.equal(expectedArray.length, data.length);
+            assert.equal(expectedArray.length, data2.length);
+
+            assert.deepEqual(expectedArray, data);
+            assert.deepEqual(expectedArray, data2);
+            done();
         });
 
     });
