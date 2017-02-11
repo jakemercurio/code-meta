c42055cb7ca843ac8f769ed999cf1787d98b1bd1
jakemercurio <jake@videoblocks.com>
2017-02-11 13:33:15 -0500

    Define fileMeta object

---
+++ src/fileMeta.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let fileMeta = {
+    name: null,
+    namespace: null,
+    path: null,
+    lines: 0,
+    classes: [],
+    imports: [],
+    variables: [],
+    functions: []
+};
+
+module.exports = fileMeta;
