cc3a6b5b2bf569fe4c12861e1728212482b5c9c8
jakemercurio <jake@videoblocks.com>
2017-04-30 13:49:23 -0400

    Remove comments without multi-line flag

--- src/php/phpTokenizer.js
+++ src/php/phpTokenizer.js
@@ -12,7 +12,7 @@ class PhpTokenize {
     }
 
     static _removeComments(code) {
-        return code.replace(/(\/\*[\w\W]*?\*\/)|(\/\/[\w\W]*?\n)|(<!--[\W\w]*?-->)/gm, '');
+        return code.replace(/(\/\*[\w\W]*?\*\/)|(\/\/[\w\W]*?\n)|(<!--[\W\w]*?-->)/g, '');
     }
 
     getRawCode() {
